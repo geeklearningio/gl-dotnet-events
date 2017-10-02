11232d88d80c8689695c7e6d9df7976a44453f7b
Ronan Janvresse <ronan.janvresse@bpce.fr>
2017-10-02 17:17:28 +0200

    Add project files.

---
+++ GeekLearning.Events.Storage/StorageOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeekLearning.Events.Storage
+{
+    public class StorageOptions
+    {
+        public string ConnectionString { get; set; }
+
+        public string QueueName { get; set; }
+    }
+}
---
+++ GeekLearning.Events/IEventQueuer.cs
@@ -0,0 +1,12 @@
+﻿namespace GeekLearning.Events
+{
+    using GeekLearning.Events.Model;
+    using System.Threading.Tasks;
+
+    public interface IEventQueuer
+    {
+        void QueueEvent<TEvent>(TEvent Event) where TEvent : EventBase;
+
+        Task CommitAsync();
+    }
+}
